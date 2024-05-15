da08ac8dee6eae90a171a3c9daa328b3f88415ed
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-15 08:02:23 -0300

    stripes on the background

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import "./css/App.css";
 function App() {
     return (
         <main className="app">
+            <div className="strip left"></div>
+            <div className="strip right"></div>
             <CellPhone />
             <Text />
             <Footer />
